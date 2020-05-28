74bf667aad5e88a587664456d08ff05035c77f81
jkozma <jkozma@acm.org>
2020-05-27 20:39:04 -0400

    Add files via upload

---
+++ js/yogidata.js
@@ -0,0 +1,96 @@
+var hquot = [];
+var himg = [];
+var hcred = [];
+
+hquot.push("The problem with bottled beer is you have to get it out of the bottle to drink it.");
+//instead of using markup text as the array element use
+//newimg = document.createElement("IMG");
+//newimg.src = "bottleOpener.png";
+//newimg.alt = "bottleOpener";
+//newimg.class = "himage";
+//himg.push(newimg);
+himg.push("imgs/bottleOpener.png");
+hcred.push("Public domain, drawing from U.S. patent");
+
+hquot.push("The reason they call it Oktoberfest is you can celebrate it any time of year.");
+himg.push("imgs/BeerSanta1904PuckCover.jpg");
+hcred.push("Frank Arthur Nankivell [Public domain], <a href=\"https://commons.wikimedia.org/wiki/File:Santa_Claus_and_list_in_1904_art,_Santa1904PuckCover_(cropped).jpg\">via Wikimedia Commons</a>");
+
+hquot.push("Sometimes we start playing a polka in three, but we end up playing a waltz.");
+himg.push("imgs/polkaWaltzDancers.jpg");
+hcred.push("Benjamin Cossel [Public domain], <a href=\"https://commons.wikimedia.org/wiki/File:Dancers_celebrate_Oktoberfest_in_Guantanamo.jpg\">via Wikimedia Commons</a>");
+
+hquot.push("The three most popular dances at Oktoberfest are polka, waltz and tap.");
+himg.push("imgs/polkaWaltzTap.jpg");
+hcred.push("Anais Colin Toudouze, Le Maitre a danser (1844)");
+
+hquot.push("Very few men wear a dirndl because it's so hard to spell.");
+himg.push("imgs/dirndlEmilRauLesendesMadchen.jpg");
+hcred.push("Emil Rau (1858-1937) [Public domain], <a href=\"https://commons.wikimedia.org/wiki/File:Emil_Rau_Lesendes_M%C3%A4dchen_02.jpg\">via Wikimedia Commons</a>");
+
+hquot.push("You know your beer is too hoppy when even a frog won't drink it.");
+himg.push("imgs/frogTapBeerZapfhahnEinschenken.jpg");
+hcred.push("Benreis [<a href=\"https://creativecommons.org/licenses/by/3.0\">CC BY 3.0</a>], <a href=\"https://commons.wikimedia.org/wiki/File:Zapfhahn_einschenken.jpg\">via Wikimedia Commons</a>, Charles J Sharp [<a href=\"https://creativecommons.org/licenses/by-sa/3.0\">CC BY-SA 3.0</a>], <a href=\"https://commons.wikimedia.org/wiki/File:Gladiator_frog_Osa.JPG\">via Wikimedia Commons</a>");
+
+hquot.push("You can absorb a lot of good beer by just drinking it.");
+himg.push("imgs/ConsMunichBeer.jpg");
+hcred.push("U.S. Consulate General Munich from Germany [Public domain], <a href=\"https://commons.wikimedia.org/wiki/File:ConsMunich_Picture_033_(9365724546).jpg\">via Wikimedia Commons</a>");
+
+hquot.push("They say alcohol impairs decision making, so if you're thirsty it's best to order two beers in case you can't make up your mind after the first one.");
+himg.push("imgs/SpillingBeerTheTerracePubCropped.jpg");
+hcred.push("Kolforn [CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0)], via Wikimedia Commons\" <a href=\"https://commons.wikimedia.org/wiki/File:Spilling_the_Beer,_The_Terrace_Pub_before_the_match,27_February_2016.JPG\">via Wikimedia Commons</a>");
+
+hquot.push("Some people think German is hard to understand. The trick is to drink a couple of glasses of beer. Then it sounds just like any other language.");
+himg.push("imgs/HermannVolzSchankmagdBierStammtisch1872.jpg");
+hcred.push("Hermann Volz (1814-1894) [Public domain], <a href=\"https://commons.wikimedia.org/wiki/File:Hermann_Volz_Schankmagd_bringt_Bier_an_den_Stammtisch_1872.jpg\">via Wikimedia Commons</a>");
+
+hquot.push("Newton once visited America and tried near beer. That's how he discovered the law of gravity.");
+himg.push("imgs/IsaacNewtonLaboratoryFire.jpg");
+hcred.push("Morel [Public domain], <a href=\"https://commons.wikimedia.org/wiki/File:Isaac_Newton_laboratory_fire.jpg\">via Wikimedia Commons</a>");
+
+hquot.push("I like dark beer, but with all the new IPAs coming out, it's a return to the gold standard.");
+himg.push("imgs/1849GoldDollarOpenWreathBeerAlpha.png");
+hcred.push("Coin Image courtesy of Heritage Auctions [Public domain], Public Domain Beer Stein Image scanned Circa 1880's German Print, <a href=\"https://thegraphicsfairy.com/wp-content/uploads/2019/09/Public-Domain-Beer-Stein-Image-GraphicsFairy-740x1024.jpg\">via the Graphics Fairy</a>");
+
+hquot.push("Ninety percent of our music is all about beer. The other half is polkas and waltzes.");
+himg.push("imgs/AffligemGobletMusic.png");
+hcred.push("Affligem Global Marketing Team [Public domain], music emojis <a href=\"https://www.unicode.org/emoji/charts/full-emoji-list.html#music\">via unicode.org</a>");
+
+hquot.push("Yogi Berra once said, \"I never said most of the things I said.\" Neither did I.");
+himg.push("imgs/ThreeWiseMonkeys.jpg");
+hcred.push("Ray in Manila [<a href=\"https://creativecommons.org/licenses/by/2.0\">CC BY 2.0</a>], <a href=\"https://commons.wikimedia.org/wiki/File:The_Three_Wise_Monkeys,_Nikk%C5%8D_T%C5%8Dsh%C5%8D-g%C5%AB;_April_2018.jpg\">via Wikimedia Commons</a>");
+
+hquot.push("It's d.s. al capo all over again.");
+himg.push("imgs/ConsMunichBeerTiled.jpg");
+hcred.push("U.S. Consulate General Munich from Germany [Public domain], <a href=\"https://commons.wikimedia.org/wiki/File:ConsMunich_Picture_033_(9365724546).jpg\">via Wikimedia Commons</a>");
+
+hquot.push("Some bartenders like to make a big show of pouring a perfect glass of beer, but they're just playing with your head.");
+himg.push("imgs/FoamedWhenDispensingBeer.jpg");
+hcred.push("Ryan Vaarsi [<a href=\"https://creativecommons.org/licenses/by/2.0\">CC BY 2.0</a>], <a href=\"https://commons.wikimedia.org/wiki/File:Foamed_when_dispensing_beer.jpg\">via Wikimedia Commons</a>");
+
+hquot.push("Hans's nephew once asked his advice when preparing for a job interview at Microsoft. Hans told him, \"If you don't see sharp, you'll Bb.\"");
+himg.push("imgs/seeSharpBeFlat.png");
+hcred.push("program template <a href=\"https://github.com/icsharpcode\">via SharpDevelop</a>, <a href=\"https://www.flaticon.com/free-icon/clef_898927\">clef icon</a> designed by prettycons from Flaticon, <a href=\"https://www.flaticon.com/free-icon/nail_269920\">nail icon</a> and <a href=\"https://www.flaticon.com/free-icon/flat-tire_2061867\">flat tire icon</a> designed by Freepik from Flaticon");
+
+var i = 0;
+function showimg() {
+    i = Math.floor((Math.random() * hquot.length));
+    document.getElementById("imgdiv").style.backgroundImage = "url("+himg[i]+")";
+    document.getElementById("quotspan").innerHTML = hquot[i];
+    document.getElementById("credspan").innerHTML = "Picture credit (click)";
+}
+
+function show_link() {
+  document.getElementById("credspan").style.textDecoration = "underline";
+  document.getElementById("credspan").style.color = "blue";
+}
+function show_credit() {
+  document.getElementById("credspan").innerHTML = hcred[i];
+  document.getElementById("credspan").style.textDecoration = "none";
+  document.getElementById("credspan").style.color = "black";
+}
+function hide_credit() {
+  document.getElementById("credspan").innerHTML = "Picture credit (click)";
+  document.getElementById("credspan").style.textDecoration = "none";
+  document.getElementById("credspan").style.color = "black";
+}
